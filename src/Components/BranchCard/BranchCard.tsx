2a46d6157fcd9d88a560b67f90b07b5dc4946bda
fahime <fahimeh757@gmail.com>
2023-05-27 09:28:25 -0700

    Add BranchSection Style and Add popUpFullPictures

--- src/Components/BranchCard/BranchCard.tsx
+++ src/Components/BranchCard/BranchCard.tsx
@@ -1,25 +1,55 @@
-import { ArrowLeft2, Gallery, Image } from "iconsax-react";
+import { ArrowLeft2, Gallery } from "iconsax-react";
 import BranchCardData from "./BranchCradData";
+import expandIcon from "../../assets/branches/expand.png";
+import { useState } from "react";
+import PopUpPicture from "./PopUpPicture";
+
 function BranchCard() {
+  const [isHover, setIsHover] = useState(-1);
+  const [isPopUpPicture, setIsPopUpPicture] = useState(-1);
+  //hovering on Branch Card
+  const MouseEnter = (index: number) => {
+    setIsHover(index);
+  };
+  const MouseLeave = () => {
+    setIsHover(-1);
+  };
+  //select picture to show full size on popup
+  const openPopUp = (index: number) => {
+    setIsPopUpPicture(index);
+  };
+  const closePopUp = () => {
+    setIsPopUpPicture(-1);
+  };
   return (
     <section className="w-full flex flex-col justify-center items-center mt-16 ">
       <h1 className=" font-estedadSemiBold md:text-2xl text-gray-8 ">
         ترخینه گردی
       </h1>
-      <section className="w-full grid gap-3 px-5 py-3 md:grid-cols-2 lg:grid-cols-4">
-        {BranchCardData.map((data) => {
+      <section className="w-full grid gap-3 px-5 py-3 md:grid-cols-2 lg:grid-cols-4 mt-4">
+        {BranchCardData.map((data, index) => {
           return (
             <div
               key={data.id}
-              className="flex  justify-between w-full cursor-pointer rounded-xl border border-gray-4 overflow-hidden hover:border-primaryGreen duration-125 lg:flex-col lg:hover:border-0 lg:hover:border-gray-4 lg:hover:shadow-lg"
+              className="flex  justify-between w-full cursor-pointer lg:cursor-default rounded-xl border border-gray-4 overflow-hidden hover:border-primaryGreen duration-125 lg:flex-col lg:hover:border-0 lg:hover:border-gray-4 lg:hover:shadow-lg 
+               lg:[&>*:first-child]:hover:h-48  [&>*:first-child]:bg-black  "
+              onMouseEnter={() => MouseEnter(data.id)}
+              onMouseLeave={MouseLeave}
             >
-              <div className="relative w-36  h-20 lg:w-full lg:h-[230px] bg-black lg:hover:h-48">
+              <div className="relative w-36  h-20 lg:w-full lg:h-[230px]  duration-300 cursor-pointer ">
                 <img
-                  className=" object-cover w-full h-full lg:h-full //opacity-40"
+                  className={`object-cover w-full h-full lg:h-full ${
+                    isHover == data.id && "opacity-40"
+                  }  duration-300`}
                   src={data.imageUrl}
                   alt={data.branch}
                 />
-                <div className="absolute top-0 right-0 w-full h-full //flex justify-center items-center hidden">
+                <div
+                  className={`absolute top-0 right-0 w-full h-full  justify-center items-center hidden  lg:${
+                    isHover == data.id && "flex"
+                  } `}
+                  onClick={() => openPopUp(index)}
+                >
                   <div className=" w-14 h-14 bg-white/30 rounded-full flex justify-center items-center ">
                     {" "}
                     <div className=" w-11 h-11 bg-white/50 rounded-full flex justify-center items-center ">
@@ -29,22 +59,27 @@ function BranchCard() {
                 </div>
                 <img
                   className=" absolute right-2 bottom-2 w-4 h-4 lg:hidden"
-                  src="https://s3-alpha-sig.figma.com/img/bcd7/7ef1/e792c4724b6c0b252f0dcbf50025cb61?Expires=1684713600&Signature=pCWDAGLcoyEOALdJiVA--HusgJeGHJZXfPExVEL-PTFN0m2n0RK725GMhJoPn7BWvN6nNfhv5JbDiAxL7u6LU46vw8Xb06UdsWNBUe7D7g368w~09~sjG2vFHCBomWMzZ2rTLNptufch7wkX-I71u1aNQbak48JzHHkaoQYzdFXnNo3CQ-htbjrgvMkMOZtWSMzrFDks0tB~zH5y7lzdYt9GMdadDgXrr-KkjhLkc3LppLT9KX~WBdtuqlU6SQ5zRGBKXHgZBaWZqcoMjM9hKD7QVxh-m8zM6zCeFF3odpoamD011Hb5aiQvL4hoeWr6KeCQ6inrHssDIO11wi~iOA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  src={expandIcon}
                   alt="resize"
+                  onClick={() => openPopUp(index)}
                 />
               </div>
-              <div className="flex flex-col justify-center items-center gap-1 p-2  w-full lg:pb-4 lg:gap-3">
+              <div className="flex flex-col justify-center items-center gap-1 p-2  w-full lg:pb-4 lg:gap-3 duration-300">
                 <p className=" font-medium text-sm text-gray-8 lg:font-semibold lg:text-xl ">
                   {data.branch}
                 </p>
                 <p className=" text-[10px] text-center lg:font-medium lg:text-sm">
                   {data.branchAddress}
                 </p>
-                <button className=" relative group border  border-primaryGreen rounded text-sm  px-10 md:px-4  py-[6px] z-[1] overflow-hidden hidden ">
+                <button
+                  className={`relative group border  border-primaryGreen rounded text-sm  px-10 md:px-4  py-[6px] z-[1] overflow-hidden hidden lg:${
+                    isHover == data.id && "flex"
+                  } duration-150`}
+                >
                   <span className="w-full h-full translate-x-96   bg-primaryGreen inset-0 absolute -z-[1] group-hover:translate-x-0 duration-500"></span>
                   <div className=" flex items-center justify-between gap-5">
                     <span className="group-hover:text-white duration-500">
-                      بیشتر
+                      صفحه شعبه
                     </span>
                     <ArrowLeft2 className="hidden md:block group-hover:text-white duration-500 text-primaryGreen" />
                   </div>
@@ -54,6 +89,13 @@ function BranchCard() {
           );
         })}
       </section>
+      {isPopUpPicture !== -1 && (
+        <PopUpPicture
+          index={isPopUpPicture}
+          closePopUp={closePopUp}
+          setIsPopUpPicture={setIsPopUpPicture}
+        />
+      )}
     </section>
   );
 }
--- src/Components/BranchCard/BranchCradData.tsx
+++ src/Components/BranchCard/BranchCradData.tsx
@@ -4,35 +4,35 @@ type BranchCardDataType = {
   branchAddress: string;
   imageUrl: string;
 }[];
+import ekbatan from "../../assets/branches/ekbatan.png";
+import chalose from "../../assets/branches/chalose.png";
+import aghdasieh from "../../assets/branches/aghdasieh.png";
+import vanak from "../../assets/branches/vanak.png";
 const BranchCardData: BranchCardDataType = [
   {
     id: 1,
     branch: "شعبه اکباتان",
     branchAddress: "شهرک اکباتان، فاز ۳، مجتمع تجاری کوروش، طبقه سوم",
-    imageUrl:
-      "https://s3-alpha-sig.figma.com/img/b261/bee1/4e04e13a12171d7b7ef85748639524b8?Expires=1684713600&Signature=Ni0juyo5jF4QEBbM0-w9oB2QnEVLtY8Gd7RZm-h~rkD0efSMWDdjsg6JKPeaPOBC1qCvTRv7-D7zErrUGghkijG~XZGFnic~aKax8~ySevHw30QgxQumYtiDPTuP-00FdlMyE36Yfhe0HN8n6SLKE2S-ZkIrzcZSyfz0kQoWLmQpqRnxns9h7Nsziny1rmXKeSS21HGNOhlJC7w7iSuSov1AVBnCyjgjBqfj2Q9WbFX~NfiVcx7oQ4S5KpKQUShA-ZMlVIkd8Pvtndip5cQ~FoHbY1gDkJTwCo~8Wf4kJxDFXfadVvd7zZ1DTB5nv8Wtm~DKQ9qs~tTwKo5QVbiExg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    imageUrl: ekbatan,
   },
   {
     id: 2,
     branch: "شعبه چالوس ",
     branchAddress:
       "چالوس، خیابان ۱۷ شهریور، بعد کوچه کوروش، جنب داروخانه دکتر میلانی",
-    imageUrl:
-      "https://s3-alpha-sig.figma.com/img/1e09/aa62/e62547070cd21866a3a7fb65f0c09ed4?Expires=1684713600&Signature=MsKIIoiYFhEH8OcbA5oLOHJFyDWwfwnUhe-0QqfE6l1e40ptvgj7fpzHoMJ3vsHU3RSWrpyv1Rh91KxYXDzU~p5aESerBevLO8L9S9j6FJF6RnrwD4S1g3AZGvazfNWjulUC1O7o-wgZ9G3nHCEzWurFvcz58Ufdm54Yb7LQwrjlrsMcVe066ZA9ANI18vURYRMUYeVl9ytD6NQI5eBLXpPnnJ~i3FN7GFObo4pyVeE8hTYeVV95vN9EKGhBAn6cyTNTO9xfB7ddC8Iib3QfJwNWYPr0hT0o~QAo5XjcwSE2WBI1ToLvuHjrD2E~xQi3He1DU~bgARWIJt-BvDbpmQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    imageUrl: chalose,
   },
   {
     id: 3,
     branch: "شعبه اقدسیه",
     branchAddress: "خیابان اقدسیه ، نرسیده به میدان خیام، پلاک ۸",
-    imageUrl:
-      "https://s3-alpha-sig.figma.com/img/d5ff/09ee/82095d7111214e5657d585e634b719e5?Expires=1684713600&Signature=OGqhX-~t5fQgyInBLCXlpe4Dq1nQYwJHEt4BckLmrlpYLGx6kKJfD2RXsBIYshU13pldFKZKLyIlMdwaKNeBvl6SbSthk8SgK3fIC6xzoX6ZSKgwfrPaqKNL1Z6kNJKoBbFT1Ux1PxfunubYD7k4IK~pMZmbmv-1yD6rwYLzk7M~u79aTGLiZAxXog8QMnjTkWd3OO5n5~UJIbG8G5xnWWlQsG499WbIvytj7g26foM1rKrH5yh~bVR2KsQGYpoXHJqbhyvQ1yJpcG4YpOS3ewGcP6EKaMZ0cdZaxUEhxWIpfkuFkV6fOlVcOTeJ2tBbkCZfM89v65aeWoEEPfemCQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    imageUrl: aghdasieh,
   },
   {
     id: 4,
     branch: "شعبه ونک",
     branchAddress: "میدان ونک، خیابان فردوسی، نبش کوچه نیلوفر، پلاک ۲۶",
-    imageUrl:
-      "https://s3-alpha-sig.figma.com/img/304f/c307/284a13bf5484d9662967305806e58e8c?Expires=1684713600&Signature=Zw-L5YtQmpPjXCEFPKqbGvOzXox0PzKEoT8yp3cKYH5YDKHe5w-dOvdjpBFEI3o6DuUb8Xkzyi7CAxCQtKhG59eSuC65CxVxCfAMyWndscHYfSn9BrkDuuVvNh0O4okBGiEzk62Mjl8~n9506b1-n0hQv6IO9jccCWyTAieBPsYKgFV4xlNlDohuT-rBWNMdVnuSWYW0eKH9CCkEN26V-JXNL9Krhc5Ya0gynzmC3-lrT-xGDqluJugsJ-XHURf2NthYW3qqPaRM3QeV9eouOrdAHS1K2n6S3l22tFvysDM5Frf24N7qpIjKpkYD3T1ht0S9xhUqoBYOaeULrDUq0Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    imageUrl: vanak,
   },
 ];
 export default BranchCardData;
